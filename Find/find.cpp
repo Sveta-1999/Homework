eacdbe5b3bf0d2b5710f61fc4ea1797b2d980f11
Sveta-1999 <79526438+Sveta-1999@users.noreply.github.com>
2022-05-16 16:36:45 +0400

    Add files via upload

--- Find/find.cpp
+++ Find/find.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
 
+
+template <typename Iter, typename T>
+Iter find(Iter begin, Iter end, T& elem)
+{
+    while (begin != end)
+    {
+        if(*begin == elem)
+        {
+            return begin;
+        }
+        ++begin;
+    }
+    return begin;
+    
+}
+
+int main() {
+    std::vector<int> vec {1,2,3,4,5,6,7};
+    int n = 5;
+    auto result =  find(begin(vec), end(vec), n);
+    std::cout << *result;
+    return 0;
+}
