801f4106928c1a259aaecafb1b4a231b36411c14
Sveta-1999 <79526438+Sveta-1999@users.noreply.github.com>
2022-05-16 16:37:41 +0400

    Add files via upload

--- Advance/advance.cpp
+++ Advance/advance.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
 
+template <typename Iter>
+void advance(Iter& index, int value, std::input_iterator_tag)
+{
+    while(value != 0)
+    {
+        ++index;
+        --value;
+    }
+}
+
+template <typename Iter>
+void advance(Iter& index, int value, std::bidirectional_iterator_tag)
+{
+    while(index != value)
+    {
+        --index;
+    }
+}
+
+template <typename Iter>
+void advance(Iter& index, int value, std::random_access_iterator_tag)
+{
+    index + value;
+}
+
+template <typename Iter>
+void advance(Iter& index, int value)
+{
+    advance(index, value, typename std::__iterator_traits<Iter>::iterator_category);
+}
+
+int main() {
+    std::vector<int> vec {1,2,3,4,5,6};
+}
